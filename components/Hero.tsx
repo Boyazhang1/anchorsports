79af6cba04e433ad32dd95ec792cf0471b325dea
Boya <boyazhang123@gmail.com>
2021-12-27 16:08:59 -0800

    fixing vercel deployment errors attempt 1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <>
       <div style={{ position: 'relative', height: '70vh' }}>
-        <Image src={banner} layout="fill" objectFit="cover" />
+        <Image src={banner} layout="fill" objectFit="cover" alt="anchor basketball banner"/>
       </div>
       <div className="flex justify-center items-center h-[15vh] mt-2">
         <button className="bg-slate-700 hover:bg-slate-900 text-white font-bold py-8 px-10 rounded-lg md:text-lg">
